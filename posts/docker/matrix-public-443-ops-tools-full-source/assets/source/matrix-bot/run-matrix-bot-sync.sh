25f2b48b2a47715205e3df031dba859ed2772705
Gary <hanigege911@gmail.com>
2026-06-24 07:09:32 +0800

    Publish site updates

--- posts/docker/matrix-public-443-ops-tools-full-source/assets/source/matrix-bot/run-matrix-bot-sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-LOCK=/tmp/matrix-bot-sync.lock
-if mkdir "$LOCK" 2>/dev/null; then
-  trap 'rmdir "$LOCK"' EXIT
-  /root/data/docker_data/synapse/scripts/matrix-bot-sync.py >> /var/log/matrix-bot-sync.log 2>&1
-fi
